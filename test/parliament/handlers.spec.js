ee2d386e98caedce236013d6bc903812f613f74e
Matt Rayner <mattrayner1@gmail.com>
2017-05-15 18:18:22 +0100

    add test coverage for basic whats on functionality

--- test/parliament/handlers.spec.js
+++ test/parliament/handlers.spec.js
@@ -1,16 +1,14 @@
 // Include our testing frameworks
 const expect = require("chai").expect; // Expectations
 const sinon = require("sinon");        // Spying
-const mockery = require("mockery");    // Mocking remote connections
-
-// Include additional modules to help with testing
-const fs = require("fs");
-const Bluebird = require("bluebird");
 
 // Include the subject of our tests
 const handlers = require("../../lib/parliament/handlers");
+
 const language_strings = require("../../lib/parliament/language_strings");
 const translation_dictionary = "en-GB";
+
+const helpers = require("../support/test_helpers");
 
 describe("The handlers module", function(){
     it("is an object", function() {
@@ -35,55 +33,66 @@ describe("The handlers module", function(){
 
         describe("WhatsOnIntent", function (){
             context("with content", function(){
-                context("for the commons commons", function(){
-                    before(function (done) {
-                        let commons_content_filename = "commons_content.html";
-                        let lords_content_filename = "lords_content.html";
-                        mockery.enable({
-                            warnOnReplace: false,
-                            warnOnUnregistered: false,
-                            useCleanCache: true
-                        });
-
-                        mockery.registerMock("request-promise", function (config) {
-                            let file_path = "";
-
-                            if (config.uri.indexOf("Commons") > 0) {
-                                file_path = __dirname + "/../fixtures/" + commons_content_filename;
-                            } else {
-                                file_path = __dirname + "/../fixtures/" + lords_content_filename;
-                            }
-
-                            let response = fs.readFileSync(file_path, "utf8");
-                            return Bluebird.resolve(response.trim());
-                        });
-
-                        mocked_handlers = require("../../lib/parliament/handlers");
-                        mocked_handlers.t = function(key) { return language_strings[translation_dictionary].translation[key] };
-                        mocked_handlers.emit = function(arguments) {  };
-
-                        done()
-                    });
-
-                    after(function (done) {
-                        mockery.disable();
-                        mockery.deregisterAll();
-
-                        done()
-                    });
+                context("for both commons and lords", function(){
+                    // Mock handlers with content for both commons and lords
+                    helpers.mocked_handlers("commons_content.html", "lords_content.html");
 
                     it("emits as expected", function(done){
                         let spy = sinon.spy(mocked_handlers, "emit");
 
                         mocked_handlers["WhatsOnIntent"](function(done){
-                            expect(spy).to.have.been.calledWith(":askWithCard", "There are 45 on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?", "There are 45 on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?", "What's on at Parliament", "There are 45 on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?")
+                            expect(spy).to.have.been.calledWith(":askWithCard", "There are 45 events on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?", "There are 45 events on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?", "What's on at Parliament", "There are 45 events on at the Houses of Parliament today: 25 in the House of Commons and 20 in the House of Lords would you like to hear more?");
 
-                            done()
-                        }, done)
-                    })
-                })
-            })
+                            done();
+                        }, done);
+                    });
+                });
 
+                context('for just the commons', function(){
+                    // Mock handlers with content for commons only
+                    helpers.mocked_handlers("commons_content.html", "lords_no_content.html");
+
+                    it("emits as expected", function(done){
+                        let spy = sinon.spy(mocked_handlers, "emit");
+
+                        mocked_handlers["WhatsOnIntent"](function(done){
+                            expect(spy).to.have.been.calledWith(":askWithCard", "There are 25 events on at the Houses of Parliament today: they are all in the House of Commons would you like to hear more?", "There are 25 events on at the Houses of Parliament today: they are all in the House of Commons would you like to hear more?", "What's on at Parliament", "There are 25 events on at the Houses of Parliament today: they are all in the House of Commons would you like to hear more?");
+
+                            done();
+                        }, done);
+                    });
+                });
+
+                context("for just the lords", function(){
+                    // Mock handlers with content for commons only
+                    helpers.mocked_handlers("commons_no_content.html", "lords_content.html");
+
+                    it("emits as expected", function(done){
+                        let spy = sinon.spy(mocked_handlers, "emit");
+
+                        mocked_handlers["WhatsOnIntent"](function(done){
+                            expect(spy).to.have.been.calledWith(":askWithCard", "There are 20 events on at the Houses of Parliament today: they are all in the House of Lords would you like to hear more?", "There are 20 events on at the Houses of Parliament today: they are all in the House of Lords would you like to hear more?", "What's on at Parliament", "There are 20 events on at the Houses of Parliament today: they are all in the House of Lords would you like to hear more?");
+
+                            done();
+                        }, done);
+                    });
+                });
+            });
+
+            context("with no content", function(){
+                // Mock handlers with content for both commons and lords
+                helpers.mocked_handlers("commons_no_content.html", "lords_no_content.html");
+
+                it("emits as expected", function(done){
+                    let spy = sinon.spy(mocked_handlers, "emit");
+
+                    mocked_handlers["WhatsOnIntent"](function(done){
+                        expect(spy).to.have.been.calledWith(":tellWithCard", "There are no events at the houses of parliament today.", "What's on at Parliament", "There are no events at the houses of parliament today.");
+
+                        done();
+                    }, done);
+                });
+            });
         });
 
         describe("AMAZON.HelpIntent", function(){
@@ -92,7 +101,7 @@ describe("The handlers module", function(){
 
                 handlers["AMAZON.HelpIntent"]();
 
-                expect(spy).to.have.been.calledWith(":ask", "I can tell you what's going on at Parliament today. Simply ask 'what's on'.", "I can tell you what's going on at Parliament today. Simply ask 'what's on'.")
+                expect(spy).to.have.been.calledWith(":ask", "I can tell you what's going on at Parliament today. Simply ask 'what's on'.", "I can tell you what's going on at Parliament today. Simply ask 'what's on'.");
             })
         });
 
@@ -102,7 +111,7 @@ describe("The handlers module", function(){
 
                 handlers["LaunchRequest"]();
 
-                expect(spy).to.have.been.calledWith(":tell", "UK Parliament. You can find out what’s on in: the House of Commons, the House of Lords, or Both. Which would you like to do?")
+                expect(spy).to.have.been.calledWith(":tell", "UK Parliament. You can find out what’s on in: the House of Commons, the House of Lords, or Both. Which would you like to do?");
             })
         })
     })
